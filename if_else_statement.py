b578c80a2fee99a2ba803cdbfc7489072cdb53ac
Rendel Acain <acainrendelrey_bsit@plmun.edu.ph>
2025-02-13 22:31:29 +0800

    Submitting Python Week 04 - Laboratory # 09

--- if_else_statement.py
+++ if_else_statement.py
@@ -1,9 +1,12 @@
-user_input = input("Enter a number: ")
 
-number = int(user_input)
+
+
 
 
 try:
+	user_input = input("Enter a number: ")
+	number = int(user_input)
+
     if number % 2 == 0:
     print("The number", number, "is Even.")
 else:
